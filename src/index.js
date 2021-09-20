471115bcba8004bb10952eedbb99974a0a0dd94c
Arodriguez1030 <arodriguez1030@gmail.com>
2021-09-20 15:31:13 -0400

    added store and middleware, created task reducer file and initial reducer function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import App from './App';
+import taskReducer from './reducers/taskReducer';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+let store = createStore(taskReducer, composeEnhancers(applyMiddleware(thunk)))
 
 
-ReactDOM.render( <App />, document.getElementById('root')
+ReactDOM.render( 
+<Provider store={store}>
+  <App />
+</Provider>, 
+document.getElementById('root')
 );
 
 
---
+++ src/reducers/taskReducer.js
@@ -0,0 +1,5 @@
+export default function taskReducer(state = {tasks: []}, action) {
+
+    return state
+
+}
