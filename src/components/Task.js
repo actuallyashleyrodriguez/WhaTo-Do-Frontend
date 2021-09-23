53518aa332a3eafb58ea578298be37888df9e31f
Arodriguez1030 <arodriguez1030@gmail.com>
2021-09-23 14:45:55 -0400

    fixed  route for all tasks, created container for notes, fixed task show  page

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 const Task = (props) => {
-    console.log(props)
+    let task = props.tasks[props.match.params.id - 1]
+
     return(
-        <li>
-            {props.task.attributes.description}
-        </li>
+        <ul>
+             <li>
+                Task: {task ? task.attributes.description : null}
+            </li>
+             <li>
+                Task Category: {task ? task.attributes.category : null}
+            </li>
+            <li>
+                Task Status: {task ? task.attributes.status : null}
+            </li>
+            <li>
+            Due Date: {task ? task.attributes.date : null}
+            </li>
+            {task && task.attributes.notes.length > 0 ? <li>Notes: {task.attributes.notes[0].description}</li> : null}
+        
+        </ul>
     )
 
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Task from "./Task";
+import { Route, Link } from "react-router-dom";
 
 const Tasks = (props) => {
     return (
         <div className="task-list">{props.tasks.map(task => 
-                 <div key={task.id} className="task"><Task task={task}/></div>
-                 )}
+                 <li key={task.id} className="task">
+                     <Link to={`/tasks/${task.id}`}>{task.attributes.description}</Link>
+                     <br/></li>
+                 ) }
         </div>
         
     )
