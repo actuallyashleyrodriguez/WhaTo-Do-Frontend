a8fcea7470775604bcc38a40ebaf1b4ab76be6dc
Arodriguez1030 <arodriguez1030@gmail.com>
2021-09-23 15:53:03 -0400

    set up notes for notes page

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,7 +4,8 @@ const Notes = (props) => {
     console.log(props)
     return (
         <div className="note-list">{props.notes.map(note => 
-                <li key={note.id} className="note">{note.attributes.description}</li> )}
+                <li key={note.id} className="note">{note.attributes.description}  {note.attributes.task_id ? `-Affiliated with task # ${note.attributes.task_id}` : null}</li>  
+                )}
         </div>
 
     )
